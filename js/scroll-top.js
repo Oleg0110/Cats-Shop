c60805cb732b6d6f2f39957089cb42622ce5a1a5
Oleg0110 <oleg.buno13@gmail.com>
2021-07-19 16:01:51 +0300

    add header

---
+++ js/scroll-top.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let scrolled;
+
+let timer;
+
+let btn = document.getElementById("to-top")
+
+document.getElementById("to-top").onclick = function () {
+   scrolled = window.pageYOffset;
+   scrollToTop()
+}
+
+function scrollToTop() {
+   if (scrolled > 0) {
+      window.scrollTo(0, scrolled)
+      scrolled = scrolled - 100;
+      timer = setTimeout(scrollToTop, 10);
+   }
+   else {
+      clearTimeout(timer);
+      window.scrollTo(0, 0)
+   }
+}
+
+function getScroll() {
+   if (window.pageYOffset > 100) {
+      btn.style.display = ''
+   }
+   else {
+      btn.style.display = 'none'
+   }
+}
+
+
+window.onscroll = getScroll
+btn.style.display = 'none'
+
